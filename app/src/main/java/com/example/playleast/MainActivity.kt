8351b44be653fc0d299e923d617c98c0796d6d57
Nathan Birkett <98059854+NathanBirkett@users.noreply.github.com>
2024-02-04 18:29:00 -0500

    bluetooth works for play/pause

--- app/src/main/java/com/example/playleast/MainActivity.kt
+++ app/src/main/java/com/example/playleast/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playleast
 
+import android.Manifest
 import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.ComponentName
@@ -7,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.media.AudioManager
+import android.media.session.MediaSession
 import android.os.Build
 import android.os.Bundle
 import android.view.KeyEvent
@@ -19,6 +21,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
@@ -27,11 +30,13 @@ import com.example.playleast.ui.AppViewModelProvider
 import com.example.playleast.ui.home.HomeViewModel
 import com.example.playleast.ui.home.PlayleastApp
 import com.example.playleast.ui.theme.PlayleastTheme
+import java.time.LocalDateTime
 
 
 class MainActivity : ComponentActivity() {
-//    var viewModel = ViewModelProvider(this, AppViewModelProvider.Factory).get(HomeViewModel::class.java)
-
+    lateinit var receiver: BluetoothReceiver
+    lateinit var viewModel: HomeViewModel
+    var timer: Long = 0
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         println("onCreate")
@@ -47,36 +52,61 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        viewModel = ViewModelProvider(this, AppViewModelProvider.Factory).get(HomeViewModel::class.java)
 
+//        val receiver = MediaButtonReceiver(viewModel)
+//
+//        val filter = IntentFilter()
+//        filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED)
+//        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)
+//        filter.addAction(Intent.ACTION_MEDIA_BUTTON)
+////        filter.actionsIterator().forEach { println("filter: $it") }
+//        filter.priority = 10000
+//        ContextCompat.registerReceiver(this, receiver, filter, ContextCompat.RECEIVER_EXPORTED)
+        registerReceiver(mediaButtonReceiver, IntentFilter("com.foo.ACTION"), RECEIVER_NOT_EXPORTED)
+//
         (getSystemService(AUDIO_SERVICE) as AudioManager)
             .registerMediaButtonEventReceiver(ComponentName(this, BluetoothReceiver::class.java))
+//        MediaSession(this, "sessionn").setMediaButtonBroadcastReceiver(ComponentName(this, BluetoothReceiver::class.java))
     }
 
-    class BluetoothReceiver : BroadcastReceiver() {
-        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    val mediaButtonReceiver = object : BroadcastReceiver() {
+        @RequiresApi(Build.VERSION_CODES.O)
         override fun onReceive(context: Context, intent: Intent) {
-            val action = intent.action
-            println("intent: $intent")
-            if (Intent.ACTION_MEDIA_BUTTON == action) {
-                println("media pressed")
+            var now = java.time.Instant.now().toEpochMilli()
+            println(now - timer)
+            if (now - timer  < 1000) {
+                viewModel.playSong()
+            } else {
+                timer = now
             }
         }
     }
 
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+class BluetoothReceiver : BroadcastReceiver() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+        println("intent: $intent")
+        if (Intent.ACTION_MEDIA_BUTTON == action) {
+            println("media pressed")
+//                viewModel.playSong()
+            val myIntent = Intent()
+            myIntent.setAction("com.foo.ACTION")
+            context.sendBroadcast(myIntent)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PlayleastTheme {
-        Greeting("Android")
+        }
+        if (BluetoothDevice.ACTION_ACL_CONNECTED == action) {
+            println("connected")
+        }
     }
 }
